9c0519bd1e01466a0b8f4b3ba114c736e82211f6
Ben Thomas <ben.thomas@veeva.com>
2024-10-31 15:48:09 -0400

    Fix runQuery example

--- docs/vaultcrm/runQuery.js
+++ docs/vaultcrm/runQuery.js
@@ -2,7 +2,7 @@ let config = {
     object: "account__v", 
     fields: ["id", "name__v"],
     where: "name__v != 'Matthew'",
-    sort: ["Name ASC"],
+    sort: ["name__v ASC"],
     limit: 10
 }; 
 ds.runQuery(config).then(console.log,console.warn);
